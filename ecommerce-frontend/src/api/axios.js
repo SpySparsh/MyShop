d1b473f1fe1087fc88cc799b0fc9d277dcadc72f
SpySparsh <153936815+SpySparsh@users.noreply.github.com>
2025-11-05 21:17:17 +0530

    Change axios base URL to production endpoint

--- ecommerce-frontend/src/api/axios.js
+++ ecommerce-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://myshop-4-rjis.onrender.com/api',
   withCredentials: true // needed for refresh token cookie
 });
 export default instance;
