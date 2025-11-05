418845f812091517d012e4fd82b47cc373ac6f16
SpySparsh <153936815+SpySparsh@users.noreply.github.com>
2025-11-05 19:34:04 +0530

    Use environment variable for MongoDB connection URI

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ecommerce");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB:", mongoose.connection.name);
 
   } catch (err) {
